ec944da3faa21d38cc67e07978c1bc89dc7114f1
matthias <wi.matthias.345@gmail.com>
2026-01-03 20:03:13 +0100

    gui Klasse

---
+++ src/gui/mainframe.java
@@ -0,0 +1,59 @@
+package src.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class mainframe extends JFrame {
+    private CardLayout cLayout;
+    private JPanel cPanel;
+
+    public mainframe() {
+        this.setTitle("Tidebreaker"); // title of frame
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exits program when hitting the close button
+        this.setSize(640, 640); // sets x- and y-dimension
+        //this.setLayout(new BorderLayout());
+        //this.setVisible(true); // makes this visible
+
+        /*--game logo--*/
+        ImageIcon logo = new ImageIcon("../img/TidebreakerLogo.jpg");
+        this.setIconImage(logo.getImage());
+
+        /*--create card layout (for multiple screens)--*/
+        cLayout = new CardLayout();
+        cPanel = new JPanel(cLayout);
+
+        /*--create different screens--*/
+        titlescreen titlescreen = new titlescreen(this);
+        singleplayer singleplayer = new singleplayer(this);
+        multiplayer multiplayer = new multiplayer(this);
+        joinscreen joinscreen = new joinscreen(this);
+        hostscreen hostscreen = new hostscreen(this);
+        pregamescreen pregamescreen = new pregamescreen(this);
+        hostpregamescreen hostpregamescreen = new hostpregamescreen(this);
+        int gridSize = 15;
+        int[] ships = {1,2,3,1};
+        gamescreen gamescreen = new gamescreen(this, gridSize, ships);
+
+
+        /*--add to cPanel--*/
+        cPanel.add(titlescreen, "titlescreen");
+        cPanel.add(singleplayer, "singleplayer");
+        cPanel.add(multiplayer, "multiplayer");
+        cPanel.add(pregamescreen, "pregamescreen");
+        cPanel.add(joinscreen, "joinscreen");
+        cPanel.add(hostscreen, "hostscreen");
+        cPanel.add(gamescreen, "gamescreen");
+        cPanel.add(hostpregamescreen,"hostpregamescreen");
+        add(cPanel);
+        setVisible(true);
+        pack();
+    }
+    /*--public method to switch screens--*/
+    public void showScreen(String name) {
+        cLayout.show(cPanel, name);
+    }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(mainframe::new);
+    }
+}
+
---
+++ src/gui/multiplayer.java
@@ -0,0 +1,47 @@
+package src.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class multiplayer extends JPanel { // JPanel ist ein Standard-Container oder Leinwand um Buttons usw. gut zu platzieren
+    private mainframe frame; // Referenz auf das Hauptfenster
+    private JButton join_game;
+    private JButton host_game;
+    private JButton exit;
+
+    public multiplayer(mainframe frame) { // mainframe ist das Hauptfenster und der multiplayerscreen gibt Befehle an den mainframe
+        setLayout(new GridBagLayout()); // Bestimmt, wie Komponenten angeordnet werden, also das JPannel was erstellt wird, wird von dem GridBagLayout in die Mitte auf den multiplayerscreen gepackt
+        setOpaque(false); // Erlaubt der paintComponent-Methode den Hintergrund zu zeichnen
+        JPanel contentPanel = new JPanel(); // Erstellt das zentrale Pannel, das alle Steuerelemente bündelt. JPanel ist ein Standard-Container oder Leinwand um Buttons usw. gut zu platzieren
+        contentPanel.setOpaque(false); // Content Panel soll durchsichtig sein
+        contentPanel.setLayout(new GridLayout(0,1,10,10)); // der Layout Manager legt fest es gibt beliebig viele Zeilen, zwei Spalte und die Abstände sind 10
+        JLabel title = new JLabel("Tidebreaker");
+        title.setForeground(Color.WHITE);
+        join_game = new JButton("Join Game");
+        host_game = new JButton("Host Game");
+        exit = new JButton("   <-   ");
+        title.setFont(new Font("SansSerif", Font.BOLD,40));
+        join_game.setFont(new Font("SansSerif", Font.BOLD,20));
+        host_game.setFont(new Font("SansSerif", Font.BOLD,20));
+        exit.setFont(new Font("SansSerif", Font.BOLD,35));
+        contentPanel.add(title);
+        contentPanel.add(new JLabel(""));
+        contentPanel.add(join_game);
+        contentPanel.add(host_game);
+        contentPanel.add(exit);
+        join_game.addActionListener(e -> {frame.showScreen("joinscreen");});
+        host_game.addActionListener(e -> {frame.showScreen("hostscreen");});
+        exit.addActionListener(e -> {frame.showScreen("titlescreen");});
+        add(contentPanel); // das contentPanel wird auf das multiplayer-Panel gelegt
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) { // Graphics bündelt die notwendigen Werkzeuge und den aktuellen Zeichenzustand(Farbe, Schriftart...) und auf dem Objekt kann man Zeichenbefehle aufrufen
+        super.paintComponent(g); // alte Inhalt der Komponente wird gelöscht
+        Graphics2D g2d = (Graphics2D) g; // g wird umgewandelt in das Graphics2D Objekt
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON); // Befehl aktiviert die Kantenglättung
+        GradientPaint oceanGradient = new GradientPaint(0, 0, new Color(20, 30, 50), 0, getHeight(), new Color(0, 100, 160)); // es wird ein Objekt initialisiert das den Farbverlauf definieren soll. Struktur der Initialisierung: Startpunkt,Startfarbe,Endpunkt,Endfarbe
+        g2d.setPaint(oceanGradient); // der oceanGradient Farbverlauf soll für nachfolgende Füllbefehle verwendet werden
+        g2d.fillRect(0, 0, getWidth(), getHeight()); // Festlegung wo und wie groß der Bereich ist, der gefüllt werden soll mit getWidth(),getHeight() bekomme ich die Breite und Höhe
+    }
+}
