052bf1d2a125c3d081c0b93ce8b85db8fdfadf20
matthias <wi.matthias.345@gmail.com>
2026-01-22 18:27:07 +0100

    Javadoc

--- src/gui/colorpair.java
+++ src/gui/colorpair.java
@@ -3,11 +3,16 @@ import java.awt.Color;
 
 /**
  * Klasse zum verwalten von zwei Farbobjekten
- * @author Matthias
+ * @author Matthias Wiese
  */
 class colorpair {
     public Color color1;
     public Color color2;
+    /**
+     * Erstellt ein neues Farbpaar für einen Farbverlauf.
+     * @param color1 erstes Farbobjekt welches im Konstruktor initialisiert wird
+     * @param color2 zweites Farbobjekt welches im Konstruktor initialisiert wird
+     */
     colorpair(Color color1, Color color2) {
         this.color1 = color1;
         this.color2 = color2;
